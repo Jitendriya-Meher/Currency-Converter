de16885d57923de34bda858e5262ad63b9590a94
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2023-07-25 16:47:52 +0530

    pushing Currency Converter

---
+++ script.js
@@ -0,0 +1,229 @@
+
+const api_key = "bfc824f9fba014c4d848409e";
+const url = "https://v6.exchangerate-api.com/v6/bfc824f9fba014c4d848409e/latest/USD";
+
+const fromDropdown = document.getElementById("from");
+const toDropdown = document.getElementById("to");
+
+currencies = [
+    "AED",
+    "AFN",
+    "ALL",
+    "AMD",
+    "ANG",
+    "AOA",
+    "ARS",
+    "AUD",
+    "AWG",
+    "AZN",
+    "BAM",
+    "BBD",
+    "BDT",
+    "BGN",
+    "BHD",
+    "BIF",
+    "BMD",
+    "BND",
+    "BOB",
+    "BRL",
+    "BSD",
+    "BTC",
+    "BTN",
+    "BWP",
+    "BYN",
+    "BZD",
+    "CAD",
+    "CDF",
+    "CHF",
+    "CLF",
+    "CLP",
+    "CNY",
+    "COP",
+    "CRC",
+    "CUC",
+    "CUP",
+    "CVE",
+    "CZK",
+    "DJF",
+    "DKK",
+    "DOP",
+    "DZD",
+    "EGP",
+    "ERN",
+    "ETB",
+    "EUR",
+    "FJD",
+    "FKP",
+    "GBP",
+    "GEL",
+    "GGP",
+    "GHS",
+    "GIP",
+    "GMD",
+    "GNF",
+    "GTQ",
+    "GYD",
+    "HKD",
+    "HNL",
+    "HRK",
+    "HTG",
+    "HUF",
+    "IDR",
+    "ILS",
+    "IMP",
+    "INR",
+    "IQD",
+    "IRR",
+    "ISK",
+    "JEP",
+    "JMD",
+    "JOD",
+    "JPY",
+    "KES",
+    "KGS",
+    "KHR",
+    "KMF",
+    "KPW",
+    "KRW",
+    "KWD",
+    "KYD",
+    "KZT",
+    "LAK",
+    "LBP",
+    "LKR",
+    "LRD",
+    "LSL",
+    "LYD",
+    "MAD",
+    "MDL",
+    "MGA",
+    "MKD",
+    "MMK",
+    "MNT",
+    "MOP",
+    "MRO",
+    "MRU",
+    "MUR",
+    "MVR",
+    "MWK",
+    "MXN",
+    "MYR",
+    "MZN",
+    "NAD",
+    "NGN",
+    "NIO",
+    "NOK",
+    "NPR",
+    "NZD",
+    "OMR",
+    "PAB",
+    "PEN",
+    "PGK",
+    "PHP",
+    "PKR",
+    "PLN",
+    "PYG",
+    "QAR",
+    "RON",
+    "RSD",
+    "RUB",
+    "RWF",
+    "SAR",
+    "SBD",
+    "SCR",
+    "SDG",
+    "SEK",
+    "SGD",
+    "SHP",
+    "SLL",
+    "SOS",
+    "SRD",
+    "SSP",
+    "STD",
+    "STN",
+    "SVC",
+    "SYP",
+    "SZL",
+    "THB",
+    "TJS",
+    "TMT",
+    "TND",
+    "TOP",
+    "TRY",
+    "TTD",
+    "TWD",
+    "TZS",
+    "UAH",
+    "UGX",
+    "USD",
+    "UYU",
+    "UZS",
+    "VEF",
+    "VES",
+    "VND",
+    "VUV",
+    "WST",
+    "XAF",
+    "XAG",
+    "XAU",
+    "XCD",
+    "XDR",
+    "XOF",
+    "XPD",
+    "XPF",
+    "XPT",
+    "YER",
+    "ZAR",
+    "ZMW",
+    "ZWL",
+  ];
+
+  
+// create dropdown from currencies array
+currencies.forEach( currency => {
+    const option = document.createElement("option");
+    option.value = currency;
+    option.text = currency;
+    fromDropdown.add(option);
+});
+
+currencies.forEach( currency => {
+    const option = document.createElement("option");
+    option.value = currency;
+    option.text = currency;
+    toDropdown.add(option);
+});
+
+
+// setting default values
+fromDropdown.value = "USD";
+toDropdown.value = "INR";
+
+// conersion
+const convert = async () => {
+    // create references
+    const amount = document.querySelector("#amount");
+    const from = fromDropdown.value;
+    const to = toDropdown.value;
+
+    if( amount.value.length == 0){
+        alert("Please fill in the amount");
+        return;
+    }
+
+    const respone = await fetch(url);
+    const data = await respone.json();
+    console.log(data);
+
+    let fromExchange = data.conversion_rates[from];
+    let toExchange = data.conversion_rates[to];
+
+    let convertedAmount = (amount.value / fromExchange) * toExchange;
+
+    const result = document.querySelector(".result");
+    result.innerHTML = `${amount.value} ${from} = ${convertedAmount.toFixed(2)} ${to}`
+}
+convert();
+
+const btn = document.querySelector("#convert");
+btn.addEventListener("click",convert);
